c675963deec3e71b9040d8d19cc15738e9f0cac0
Adam Berger <adam@simplystated.dev>
2023-07-03 10:16:04 -0400

    feat: export types

--- src/index.ts
+++ src/index.ts
@@ -4,112 +4,162 @@ svc.OpenAPI.CREDENTIALS = "include";
 svc.OpenAPI.WITH_CREDENTIALS = true;
 
 export const config = (token: string) => {
-    svc.OpenAPI.TOKEN = token;
+  svc.OpenAPI.TOKEN = token;
 };
 
 export const machines = {
-    create: (machineName: string, signal?: AbortSignal) => withAbort(svc.MachinesService.postMachines({
+  create: (machineName: string, signal?: AbortSignal): Promise<void> =>
+    withAbort(
+      svc.MachinesService.postMachines({
         requestBody: {
-            slug: machineName,
-        }
-    }), signal),
+          slug: machineName,
+        },
+      }),
+      signal
+    ),
 };
 
 export const machineVersions = {
-    provisionallyCreateVersion: (machineName: svc.MachineSlug, signal?: AbortSignal) => withAbort(svc.MachineVersionsService.postMachinesV({
+  provisionallyCreateVersion: (
+    machineName: MachineSlug,
+    signal?: AbortSignal
+  ): Promise<ProvisionallyCreateVersionResponse> =>
+    withAbort(
+      svc.MachineVersionsService.postMachinesV({
         machineSlug: machineName,
-        requestBody: {}
-    }), signal),
+        requestBody: {},
+      }),
+      signal
+    ),
 
-    finalizeVersion: (
-        machineName: svc.MachineSlug,
-        signedMachineVersionId: svc.SignedMachineVersionId,
-        req: FinalizeVersionRequest,
-        signal?: AbortSignal
-    ) => withAbort(svc.MachineVersionsService.putMachinesV({
+  finalizeVersion: (
+    machineName: MachineSlug,
+    signedMachineVersionId: SignedMachineVersionId,
+    req: FinalizeVersionRequest,
+    signal?: AbortSignal
+  ): Promise<FinalizeVersionResponse> =>
+    withAbort(
+      svc.MachineVersionsService.putMachinesV({
         machineSlug: machineName,
         signedMachineVersionId,
         requestBody: req,
-    }), signal),
+      }),
+      signal
+    ),
 
-    createVersion: async (
-        machineName: svc.MachineSlug,
-        req: NonNullable<FinalizeVersionRequest> & { code: string },
-        signal?: AbortSignal
-    ) => {
-        const provisionalCreationRes = await machineVersions.provisionallyCreateVersion(machineName, signal);
-        const { codeUploadFields, codeUploadUrl, machineVersionId: signedMachineVersionId } = provisionalCreationRes;
+  createVersion: async (
+    machineName: MachineSlug,
+    req: NonNullable<FinalizeVersionRequest> & { code: string },
+    signal?: AbortSignal
+  ): Promise<FinalizeVersionResponse> => {
+    const provisionalCreationRes =
+      await machineVersions.provisionallyCreateVersion(machineName, signal);
+    const {
+      codeUploadFields,
+      codeUploadUrl,
+      machineVersionId: signedMachineVersionId,
+    } = provisionalCreationRes;
 
-        const uploadForm = new FormData();
-        for (const [key, value] of Object.entries(codeUploadFields)) {
-            uploadForm.append(key, value as string);
-        }
-        uploadForm.set("content-type", "application/javascript");
-        uploadForm.append(
-            "file",
-            new Blob([req.code], {
-                type: "application/javascript",
-            }),
-            `${machineName}.js`
-        );
-
-        await fetch(
-            codeUploadUrl,
-            {
-                method: "POST",
-                body: uploadForm,
-                signal,
-            },
-        );
-
-        return machineVersions.finalizeVersion(
-            machineName,
-            signedMachineVersionId,
-            {
-                clientInfo: req.clientInfo,
-                makeCurrent: req.makeCurrent,
-            },
-            signal,
-        );
+    const uploadForm = new FormData();
+    for (const [key, value] of Object.entries(codeUploadFields)) {
+      uploadForm.append(key, value as string);
     }
+    uploadForm.set("content-type", "application/javascript");
+    uploadForm.append(
+      "file",
+      new Blob([req.code], {
+        type: "application/javascript",
+      }),
+      `${machineName}.js`
+    );
+
+    await fetch(codeUploadUrl, {
+      method: "POST",
+      body: uploadForm,
+      signal,
+    });
+
+    return machineVersions.finalizeVersion(
+      machineName,
+      signedMachineVersionId,
+      {
+        clientInfo: req.clientInfo,
+        makeCurrent: req.makeCurrent,
+      },
+      signal
+    );
+  },
 };
 
 export const machineInstances = {
-    create: (
-        machineName: svc.MachineSlug,
-        instanceName: svc.MachineInstanceSlug,
-        req: CreateMachineInstanceRequest,
-        signal?: AbortSignal
-    ) => withAbort(svc.MachineInstancesService.postMachines({
+  create: (
+    machineName: MachineSlug,
+    instanceName: MachineInstanceSlug,
+    req: CreateMachineInstanceRequest,
+    signal?: AbortSignal
+  ): Promise<State> =>
+    withAbort(
+      svc.MachineInstancesService.postMachines({
         machineSlug: machineName,
         requestBody: {
-            slug: instanceName,
-            context: req?.context,
-            machineVersionId: req?.machineVersionId,
-        }
-    }), signal),
+          slug: instanceName,
+          context: req?.context,
+          machineVersionId: req?.machineVersionId,
+        },
+      }),
+      signal
+    ),
 
-    sendEvent: (
-        machineName: svc.MachineSlug,
-        instanceName: svc.MachineInstanceSlug,
-        req: NonNullable<SendEventRequest>,
-        signal?: AbortSignal
-    ) => withAbort(svc.MachineInstancesService.postMachinesIEvents({
+  sendEvent: (
+    machineName: MachineSlug,
+    instanceName: MachineInstanceSlug,
+    req: NonNullable<SendEventRequest>,
+    signal?: AbortSignal
+  ): Promise<State> =>
+    withAbort(
+      svc.MachineInstancesService.postMachinesIEvents({
         machineSlug: machineName,
         instanceSlug: instanceName,
         requestBody: {
-            event: req.event,
-        }
-    }), signal),
+          event: req.event,
+        },
+      }),
+      signal
+    ),
 };
 
-export type FinalizeVersionRequest = Parameters<typeof svc.MachineVersionsService.putMachinesV>[0]["requestBody"];
-export type CreateMachineInstanceRequest = Parameters<typeof svc.MachineInstancesService.postMachines>[0]["requestBody"];
-export type SendEventRequest = Parameters<typeof svc.MachineInstancesService.postMachinesIEvents>[0]["requestBody"];
+export type FinalizeVersionRequest = Parameters<
+  typeof svc.MachineVersionsService.putMachinesV
+>[0]["requestBody"];
+export type CreateMachineInstanceRequest = Parameters<
+  typeof svc.MachineInstancesService.postMachines
+>[0]["requestBody"];
+export type SendEventRequest = Parameters<
+  typeof svc.MachineInstancesService.postMachinesIEvents
+>[0]["requestBody"];
 
-function withAbort<T>(cancellablePromise: svc.CancelablePromise<T>, signal?: AbortSignal) {
-    if (signal) {
-        signal.addEventListener("abort", () => cancellablePromise.cancel());
-    }
-    return cancellablePromise;
+export type State = svc.State;
+export type ProvisionallyCreateVersionResponse = Awaited<
+  ReturnType<typeof svc.MachineVersionsService.postMachinesV>
+>;
+export type FinalizeVersionResponse = Awaited<
+  ReturnType<typeof svc.MachineVersionsService.putMachinesV>
+>;
+
+export type MachineSlug = svc.MachineSlug;
+export type MachineInstanceSlug = svc.MachineInstanceSlug;
+export type SignedMachineVersionId = svc.SignedMachineVersionId;
+export type MachineVersionId = svc.MachineVersionId;
+export type Event = svc.Event;
+export type EventWithPayload = svc.EventWithPayload;
+export type EventWithoutPayload = svc.EventWithoutPayload;
+
+function withAbort<T>(
+  cancellablePromise: svc.CancelablePromise<T>,
+  signal?: AbortSignal
+) {
+  if (signal) {
+    signal.addEventListener("abort", () => cancellablePromise.cancel());
+  }
+  return cancellablePromise;
 }
