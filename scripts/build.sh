b7b04cbf3bbfe9efcf7be2d34d5c9fbb1353fb97
Adam Berger <adam@simplystated.dev>
2023-07-03 17:29:48 -0400

    build: run ci without docker

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,21 @@
 #!/bin/bash -e
 
+cmd="run --allow-env --allow-net=deno.land,registry.npmjs.org --allow-run --allow-read --allow-write=npm,/home/${USER}/.cache/esbuild ./scripts/build.ts"
+
+docker='1'
+
+if [ "$1" = "--no-docker" ]; then
+    shift
+    docker='0'
+fi
+
 if [ -z "$1" ]; then
     echo "Usage: $0 <version>"
     exit 1
+fi
+
+if [ "$docker" = "0" ]; then
+    exec deno $cmd "$@"
 fi
 
 tag="statebacked-build:1"
@@ -24,4 +37,4 @@ EOF
 
 fi
 
-exec docker run --rm -e "DENO_DIR=/home/${USER}/deno_dir" -v "$(pwd)/deno_dir:/home/${USER}/deno_dir" -v "$(pwd):/home/${USER}/code" --workdir="/home/${USER}/code" --user "${USER}" --entrypoint=/root/.deno/bin/deno ${tag} run --allow-env --allow-net=deno.land,registry.npmjs.org --allow-run --allow-read --allow-write="npm,/home/${USER}/.cache/esbuild" ./scripts/build.ts "$@"
+exec docker run --rm -e "DENO_DIR=/home/${USER}/deno_dir" -v "$(pwd)/deno_dir:/home/${USER}/deno_dir" -v "$(pwd):/home/${USER}/code" --workdir="/home/${USER}/code" --user "${USER}" --entrypoint=/root/.deno/bin/deno ${tag} $cmd
--- scripts/gen.sh
+++ scripts/gen.sh
@@ -1,3 +1,9 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
-exec docker run --rm -e DENO_DIR=/home/deno/deno_dir -v "$(pwd)/deno_dir:/home/deno/deno_dir" -v "$(pwd):/home/deno/code" --workdir=/home/deno/code --user "${UID}" denoland/deno:ubuntu-1.34.3 run --allow-read --allow-write=src/gen-api.ts ./scripts/gen.ts
+cmd="run --allow-read --allow-write=src/gen-api.ts ./scripts/gen.ts"
+
+if [ "$1" = "--no-docker" ]; then
+    exec deno $cmd
+fi
+
+exec docker run --rm -e DENO_DIR=/home/deno/deno_dir -v "$(pwd)/deno_dir:/home/deno/deno_dir" -v "$(pwd):/home/deno/code" --workdir=/home/deno/code --user "${UID}" denoland/deno:ubuntu-1.34.3 $cmd
